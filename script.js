90802f91532e676fb069dbdb8ad842c15c6af29d
Katelyn Molina <82613638+katelynmolina@users.noreply.github.com>
2021-04-17 02:30:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Warning! This message will self-desrtuct in");
+
+
+alert("3...");alert("2...");alert("1...");
+  
+
+
+document.querySelector("h1").textContent = "🔥BOOM!🔥";
+
+console.log("Message destroyed!");
